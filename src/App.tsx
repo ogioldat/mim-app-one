815849d08dc204697e543654c8c5eeaa0e78a69c
ogioldat <tomasz.ogiolda00@gmail.com>
2023-04-13 22:42:25 +0200

    remove ariakit

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,34 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import * as Ariakit from "@ariakit/react";
-
 
 function App() {
-  const dialog = Ariakit.useDialogStore();
+  const [count, setCount] = useState(0)
+
   return (
-    <>
-      <Ariakit.Button onClick={dialog.toggle}>Open dialog</Ariakit.Button>
-      <Ariakit.Dialog store={dialog}>
-        <Ariakit.DialogHeading>Welcome</Ariakit.DialogHeading>
-        <Ariakit.DialogDescription>
-          Welcome to Reakit!
-        </Ariakit.DialogDescription>
-      </Ariakit.Dialog>
-    </>
-  );
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
 }
+
 export default App
